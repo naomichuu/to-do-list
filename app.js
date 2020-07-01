f9938df9c72b53c6954cf1ae1347954a5c920c2a
Naomi Chu <naomichu@Naomis-MBP.attlocal.net>
2020-07-01 01:16:01 -0500

    fix error4

--- app.js
+++ app.js
@@ -165,6 +165,11 @@ app.post("/work",function(req,res){
 })
 */
 
-app.listen(process.env.PORT || 2000, function(){
-  console.log("Server started on port 2000");
-});
+  let port = process.env.PORT;
+  if (port == null || port == "") {
+    port = 2000;
+  }
+
+  app.listen(port, function() {
+    console.log("Server started succesfully");
+  });
