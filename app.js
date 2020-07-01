75d39be606f02317233c408bb55eab80aa9f0bb1
Naomi Chu <naomichu@Naomis-MBP.attlocal.net>
2020-07-01 00:14:09 -0500

    fix error2

--- app.js
+++ app.js
@@ -170,6 +170,6 @@ app.post("/work",function(req,res){
 })
 */
 
-app.listen(process.env.PORT || 3000, function(){
+app.listen(process.env.PORT || 2000, function(){
   console.log("Server started on port 2000");
 });
